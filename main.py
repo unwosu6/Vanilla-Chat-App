c1869d66cd26eda215529a037f0f591e1853be2a
Arianna Hamilton <ariannahamilton@gmail.com>
2021-07-29 15:37:35 +0000

    change header for profile page
    Co-authored-by: Arianna Hamilton <ariannahamilton@gmail.com>
    Co-authored-by: Christian Santana <christians1121@gmail.com>
    Co-authored-by: Mario Rivero <mariorivero051@gmail.com>

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class AllGroupChats(db.Model):
     private = db.Column(db.Boolean, nullable=False)
     time_created = db.Column(db.DateTime)
     description = db.Column(db.String(120))
-    owner = db.Column(db.Integer, db.ForeignKey('User.id'))
+    # owner = db.Column(db.Integer, db.ForeignKey('User.id'))
     
     def __repr__(self):
         return f"Chat_Room('{self.chatname}', '{self.num_users}', '{self.time_created}')" 
@@ -60,6 +60,7 @@ class AllGroupChats(db.Model):
 def home():
     return render_template('home.html')
 
+@app.route("/")
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -150,7 +151,7 @@ def profile():
 
 @app.route("/<chat_id>")
 @login_required
-def profile():
+def chat():
     return render_template('chat.html', name=current_user.username)
 
 
