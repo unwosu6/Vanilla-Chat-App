e3632e51955db918646f07aecc30515abb4dbf24
unwosu6 <70655508+unwosu6@users.noreply.github.com>
2021-07-30 15:46:49 -0700

    update chat.js

---
+++ static/chat.js
@@ -0,0 +1,32 @@
+$(function(){
+    var $allMessages = $('#all-msgs')
+    var $user_id = $('#hidden-user-id').text()
+    var $chat_id = $('#hidden-chat-id').text()
+    $.ajax({
+        type: 'GET',
+        url: '/api/chat/' + $chat_id + '/messages',
+        success: function(allMessages) {
+            $.each(allMessages, function(i, msg) {
+                if (msg.user_sent_id != $user_id) {
+                    $allMessages.append(
+                        '<div class="incoming_msg">' +
+                        '<div class="incoming_msg_img"> <img src="' + msg.user_sent_pfp + '" alt="user-profie-pic"> </div>' +
+                        '<div class="received_msg">' +
+                        '<div class="received_withd_msg">' +
+                        '<p>' + msg.content + '</p>' +
+                        '<span class="time_date">' + msg.user_sent_display_name + '    |    ' + msg.time + '    |    ' + msg.date + '</span></div>' +
+                        '</div></div>'
+                    );
+                } else {
+                    $allMessages.append(
+                        '<div class="outgoing_msg">' +
+                        '<div class="sent_msg">' +
+                        '<p>' + msg.content + '</p>' +
+                        '<span class="time_date">' + msg.time + '    |    ' + msg.date + '</span></div>' +
+                        '</div>'
+                    );
+                }
+             });
+        }
+    });
+});
